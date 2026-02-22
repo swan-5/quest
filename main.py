684ba1f4bdcc88de212e6cb691150a3b0d9af591
swan <tjdhksld@gmail.com>
2026-02-22 14:52:57 +0900

    ver3

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 n = int(input())
-l1 = []
-l2 = []
+l= []
+lm = []
+ls = []
 t1 = 0
 t2 = 0
 
 for i in range(n):
         t,a = input().split()
         t = int(t)
-        l1.append(t)
+        l.append(t)
         m,s = str(a).split(':')
-        l2.append(m)
+        m,s = int(m), int(s)
+        lm.append(m) #골 넣은 분
+        ls.append(s) #초
         
-        
+#농구는 48분동안 진행된다. 
+for i in range(len(l)):
+        if l[i] == 1:
+                t1 += 1
+                if t1 > t2 :
+                        lm[i]
 
 
-print(l1,l2)
+
+
+print(l,lm,ls)
