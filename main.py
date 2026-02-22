0276a57661935f874ee1f6b0f5c9c50973b3fb24
swan <tjdhksld@gmail.com>
2026-02-22 14:23:30 +0900

    ver2

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ t2 = 0
 for i in range(n):
         t,a = input().split()
         t = int(t)
-        l.append(t)
+        l1.append(t)
+        m,s = str(a).split(':')
+        l2.append(m)
+        
+        
 
 
-print(l)
+print(l1,l2)
