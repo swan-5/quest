5ecd49303b05fe64eabfa6e22bd8e13ec68e641d
swan <tjdhksld@gmail.com>
2026-02-26 15:47:14 +0900

    2512

--- sub.py
+++ sub.py
@@ -1,13 +1,18 @@
 n = int(input())
 l = list(map(int,input().split()))
-l.sort()
 m = int(input())
 total = sum(l)
+b = [] # m//n 보다 큰 애들
+s = []
 
 if total <= m :
     print(max(l))
 
 else : 
-
-    d = total - m 
+    d = total - m  
     for i in range(len(l)):
+        if l[i] >= m//n : 
+            b.append(l[i])
+        else : 
+            s.append(l[i])
+    print((m -sum(s)) // len(b))
